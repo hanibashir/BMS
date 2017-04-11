868cb1ec3a89653b45f67ce2cefc6e998a4e7c7b
hbalassan <hbalhassan@gmail.com>
2017-04-11 13:25:42 +0200

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -6,9 +6,9 @@
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
    <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-<script type="text/javascript" src="assets/js/slider.js"></script>
+<script type="text/javascript" src="includes/js/slider.js"></script>
 <script src="http://malsup.github.com/jquery.form.js"></script>
-<script type="text/javascript" src="assets/js/form.js"></script>
+<script type="text/javascript" src="includes/js/form.js"></script>
 <script src="https://use.fontawesome.com/f6acdc4c89.js"></script>
 
 <?php
